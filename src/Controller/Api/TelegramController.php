1a02793767bd74964a5cb94d26e8574dabf3246b
Sven Jostkleigrewe <sven@jostkleigrewe.com>
2023-10-16 19:35:28 +0200

    add webhook endpoint

--- src/Controller/Api/TelegramController.php
+++ src/Controller/Api/TelegramController.php
@@ -5,12 +5,12 @@ namespace Jostkleigrewe\TelegramCoreBundle\Controller\Api;
 //use App\Dto\Tests\TestMappingDto;
 use Jostkleigrewe\TelegramCoreBundle\Service\TelegramService;
 use Jostkleigrewe\TelegramCoreBundle\Service\TelegramWebhookService;
-use OpenApi\Attributes as OA;
+//use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[OA\Tag(name: 'telegram')]
+//#[OA\Tag(name: 'telegram')]
 //#[IsGranted('ROLE_USER')]
 class TelegramController extends AbstractController
 {
